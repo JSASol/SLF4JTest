76fd6dda425860cbaf892ebec40851bc357a4766
s-nakamura <s-nakamura@jsa.co.jp>
2015-06-26 11:28:00 +0900

    First commit.

---
+++ src/main/java/com/jsa/sample/logging/LogbackSample.java
@@ -0,0 +1,26 @@
+package com.jsa.sample.logging;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * SLF4J + Logback Sample Program
+ */
+public class LogbackSample {
+  // Logger
+  private static Logger logger;
+
+  /**
+   * Entry point
+   * @param args
+   */
+  public static void main(String[] args){
+    // Create Logger
+    logger = LoggerFactory.getLogger(LogbackSample.class);
+    
+    logger.error("Error");
+    logger.warn("Warn");
+    logger.info("Info");
+    logger.debug("Debug");
+    logger.trace("Trace");
+  }
+}
